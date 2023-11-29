309403a04583f5775f736c402618b3009c794c80
krasnovskiy_ds <80139283+ReducetoLover@users.noreply.github.com>
2023-11-29 16:04:53 +0700

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,9 +15,33 @@ namespace ASPLearn.Controllers
 
         public IActionResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "GoodMorning" : "Good Afternoon";
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
             return View();
         }
-
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //Обнаружена ошибка проверки достоверности.
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPLearn.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public required string Name { get; set; }
+        [Required(ErrorMessage = "Please enter you email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Please enter a valid email address")]
+        public required string Email { get; set; }
+        [Required(ErrorMessage = "Please enter you phone numder")]
+        public required string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
